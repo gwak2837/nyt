0fe2f329d41e9f35b72c8e1e9c7b104da6a0033f
Taeuk Gwak <gwak2837@kakao.com>
2023-11-07 16:22:13 +0900

    style: 텍스트 스타일 수정

--- src/app/NavLink.tsx
+++ src/app/NavLink.tsx
@@ -13,19 +13,21 @@ type Props = {
 export default function NavLink({ href = '/' }: Props) {
   const pathname = usePathname()
 
+  const labelStyle = `text-[10px] leading-3 ${pathname === href ? '' : 'text-[#6D6D6D]'}`
+
   return (
     <Link href={href} className="grid gap-[9px] p-4 text-center">
       {match(href)
         .with('/', () => (
           <>
             <HomeIcon width="24" className="mx-auto" selected={pathname === href} />
-            <span className={pathname === href ? '' : 'text-[#6D6D6D]'}>홈</span>
+            <label className={labelStyle}>홈</label>
           </>
         ))
         .with('/scrap', () => (
           <>
             <ScrapIcon width="24" className="mx-auto" selected={pathname === href} />
-            <span className={pathname === href ? '' : 'text-[#6D6D6D]'}>스크랩</span>
+            <label className={labelStyle}>스크랩</label>
           </>
         ))
         .otherwise(() => null)}
