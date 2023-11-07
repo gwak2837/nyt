f7af74df8d3f83dcf66d5161e0cb953bc7fd6458
Taeuk Gwak <gwak2837@kakao.com>
2023-11-07 15:35:34 +0900

    feat: 각종 아이콘 생성

---
+++ src/svg/ScrapIcon.tsx
@@ -0,0 +1,27 @@
+type Props = {
+  width: string
+  className?: string
+  selected?: boolean
+}
+
+export default function ScrapIcon({ width, className, selected }: Props) {
+  return (
+    <svg
+      width={width}
+      height={width}
+      viewBox="0 0 24 24"
+      className={className}
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect x="3" y="2" width="18" height="20" rx="2" stroke={selected ? 'white' : '#6D6D6D'} stroke-width="2" />
+      <path
+        d="M8 7H16M8 11.5H16M8 16H13.2"
+        stroke={selected ? 'white' : '#6D6D6D'}
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  )
+}
