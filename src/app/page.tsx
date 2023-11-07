6c967e9ba8e9f026edf3168f2b6e45dff5c4ff65
Taeuk Gwak <gwak2837@kakao.com>
2023-11-07 15:36:03 +0900

    WIP: 임시 페이지 내용 설정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,50 @@
 import Image from 'next/image'
+import Link from 'next/link'
+import HomeIcon from '@/svg/HomeIcon'
+import ScrapIcon from '@/svg/ScrapIcon'
+import NavLink from '@/app/NavLink'
+import { type PageProps } from '@/common/types'
 
-export default function Home() {
-  return <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+export default function Home({ params, searchParams }: PageProps) {
+  return (
+    <main>
+      <div>sdf</div>
+      <div>sdf</div>
+      <div>sdf</div>
+      <div>sdf</div>
+      <div>sdf</div>
+      <div>sdf</div>
+      <div>sdf</div>
+      <div>sdf</div>
+      <div>sdf</div>
+      <div>sdf</div>
+      <div>sdf</div>
+      <div>sdf</div>
+      <div>sdf</div>
+      <div>sdf</div>
+      <div>sdf</div>
+      <div>sdf</div>
+      <div>sdf</div>
+      <div>sdf</div>
+      <div>sdf</div>
+      <div>sdf</div>
+      <div>sdf</div>
+      <div>sdf</div>
+      <div>sdf</div>
+      <div>sdf</div>
+      <div>sdf</div>
+      <div>sdf</div>
+      <div>sdf</div>
+      <div>sdf</div>
+      <div>sdf</div>
+      <div>sdf</div>
+      <div>sdf</div>
+      <div>sdf</div>
+      <div>sdf</div>
+      <div>sdf</div>
+      <div>sdf</div>
+      <div>sdf</div>
+      <div>sdf</div>
+    </main>
+  )
 }
